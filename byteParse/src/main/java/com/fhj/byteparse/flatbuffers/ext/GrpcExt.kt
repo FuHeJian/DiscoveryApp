f0ad89f2686c9f6d472c7ead7bbed4fdec9835c1
fuhejian <1227577349@qq.com>
2025-06-23 23:24:04 +0800

    update

--- byteParse/src/main/java/com/fhj/byteparse/flatbuffers/ext/GrpcExt.kt
+++ byteParse/src/main/java/com/fhj/byteparse/flatbuffers/ext/GrpcExt.kt
@@ -1,8 +1,9 @@
 package com.fhj.byteparse.flatbuffers.ext
 
 import com.fhj.byteparse.flatbuffers.Message
-import com.fhj.byteparse.flatbuffers.MessageStatus
+import com.fhj.byteparse.flatbuffers.MessageData
 import com.fhj.byteparse.flatbuffers.MessageType
+import com.fhj.byteparse.flatbuffers.TextMessage
 import com.fhj.byteparse.flatbuffers.User
 import com.google.flatbuffers.FlatBufferBuilder
 import java.util.Objects
@@ -23,6 +24,13 @@ fun UserMakeOffset(builder: FlatBufferBuilder, device: String, name: String, ip:
     }
 }
 
+fun TextMessageMakeOffset(builder: FlatBufferBuilder,obj: TextMessage): Int{
+    return builder.run {
+        val text = this.createString(obj.text())
+        TextMessage.createTextMessage(this,text)
+    }
+}
+
 fun MessageMake(
     type: Long,
     id: Long,
@@ -35,7 +43,15 @@ fun MessageMake(
     return FlatBufferBuilder(0).run {
         val fromUserOffset = UserMakeOffset(this,fromUser.device(),fromUser.name(),fromUser.ip())
         val toUserOffset = UserMakeOffset(this,toUser.device(),toUser.name(),toUser.ip())
-        val dataOffset = this.create(data.toString())
+        var dataType = MessageData.TextMessage
+        val dataOffset = when(type){
+            MessageType.Text ->{
+                dataType = MessageData.TextMessage
+                TextMessageMakeOffset(this,data as TextMessage)
+            }
+
+            else -> {0}
+        }
         finish(
             Message.createMessage(
                 this,
@@ -44,6 +60,7 @@ fun MessageMake(
                 fromUserOffset,
                 toUserOffset,
                 status,
+                dataType,
                 dataOffset
             )
         )
